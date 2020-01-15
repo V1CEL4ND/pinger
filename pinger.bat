452c48bc2a5dcf45bfe1f40c3bbf1a57e6f89d58
V1CE <59756115+V1CEL4ND@users.noreply.github.com>
2020-01-15 18:32:40 -0500

    Add files via upload

---
+++ pinger.bat
@@ -0,0 +1,113 @@
+# Made by V1CE, do not deface this message #
+
+@echo off
+title Made by V1CE @V1CE#0067
+cls 
+color B                                                
+echo ===================================================
+echo  #            #    # # #   # # # ##### ######
+echo  #     #   #  #       #    #     #     #   
+echo  #     #   #  #      #     # # # ##### #     
+echo  #     #   #  #    #           # #     #    
+echo  ####  #####  #### ######  # # # ##### ######
+echo ===================================================
+echo                       Loading          
+echo                    ~ IP PINGER ~              
+echo =================================================== 
+pause 
+cls
+color 2
+
+echo                         `` ``-/sydNMNNN-`                                                          
+echo                       .:+shmNNNMMMMMMMMm/`                                                         
+echo                 ``-/oymNMMMMMMMMMMMMMMMNNy.`                                                       
+echo             `-:oydNNMMMMMMMMMMMMMMMMMMMMMNy`                                                       
+echo          `:ydNNMMMMMMMMMMMMMMMMMMMMMMMMMMNNo`                                                      
+echo       .-odNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN/                                                      
+echo    ./ymNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh`                                                     
+echo `-smNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo`                                                    
+echo `+NNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNs.                                                   
+echo  `/hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNs`                                                  
+echo    .omMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNs.                   `..--                         
+echo     `:dNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy.           `.::+shhdmNy`                        
+echo       -hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNmho-   ` `.:+ydmNNMMMNMNNo`                        
+echo       `.yNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNmdysoooyh:-:/sdmNMMMMMNNmdhy+-`                         
+echo        `.hMMMMMMMMMMMMMMMMMMMMMMMMMMMNmdhsoooshdmmNNmNNNNNNMMNmdys/-```                            
+echo          .yNMMMMMMMMMMMMMMMMMMMMNmmhysooshdmNNMMMMMMMMNNmdhs+:-.`                                  
+echo           `yNMMMMMMMMMMMMMMNmmdysoosyhdNNMMMMMMMMNNNNmy+-.`                                        
+echo            `/mMMMMMMMMNNmdysoosyhdmNMMMMMMMMNNmdhhsssyys+-.``                                      
+echo            ` /dNNNNmdyssosyhdNNNMMMMMNNmmddyso+/:-.`````.:/oo:`                                    
+echo              `.yysoooshdmNNMMMMMNNmmdhyys/:/:``           ``-/o/`                                  
+echo              `./hddmNNMMMMMNNmmdhyyso+-.` /mm/.``````````.-/. .so`                                 
+echo         `.-:+ydNMMMMMMNNmdhhhyyso/../-`  ``smmdhyyyyssssshy+`  `y+                                 
+echo    ``-/+ydNNNMMMNNNNmhyhyyso/-.`````/h.    `./shhy/--:+osshd.   -y.                                
+echo ``-/smNNNMMMMMNNhsddhyyys/.``./ooo+++hm:       .sy.:+/:-+m+:h-    +o.        `                      
+echo smNMMMMMNNNmyo/.`oNdhy+-```-oo///.`.-shs.      `oy+.`` ``.`:s`    -m+      `+y+`                    
+echo +NMMNNmh+::.    -mmhy:` ``+o..//....:-/oo.     `-d+.` ` `.+s.`--:-:hy.    ``+om.                    
+echo `omh+-`        `hmyy/``..oo.-//.`.`:-:-`/y`      `/oo++/ss/:odNMNMNNNds+:-.-+dm.                    
+echo ```           :Nhho``:osm-.//.`..:-:-..:d`     ``://+oo-`omNNNhs+//dmdNNNNmms:         `//:::::::/.
+echo               /myy: :yddM://....:-:-.`-/h`   `:sdNMMMNNd/mMMN+`    os`-::-.`           .d-`` ````/
+echo               +myo`.s../hy+..`.:-:-.`::/y`  -hNNNNmhdNNMdymNh```   os                  .d.       /
+echo               +myo` :/` .ss...:-/-..:-/s.  /NMMmo.   :oo/   `/h+  `do                  .d:   ````/h
+echo               +myo` .y.  `-oo+-/-../oo/`  `mNNs.``:+.`.:/++/` /y` :d:                  `ys///+ossyh
+echo              `+dyo  `o.    `./+o+++/-``` `+NNo`` .dy`:s//+/.` .+` sy.                  `+dhhhhhhhdo
+echo               +myo` `s-            `-oyo`/mNo`   -yd/`-oo:.`     .hs`                   `.ydhhddh:`
+echo               /myo` `:/`           `:yyysNmo.     `-:````        /m/                      .oNd+:.  
+echo               -Ndy.  -o`            `.----.`                    `ss`                    ` -dMy-`   
+echo               `smyo```o.                                       `+h.                   `--:hmNN/`   
+echo                `+my/``o.                                      -sy-                  -sdyo+osmd-    
+echo                 `/yh+-s.                                   `-sy/`                 `+dy:`  ``so     
+echo                   -yhsh/``                              `./yhy.`                 `:my``     so     
+echo                    .+hmds+:``                         `:smmm+`                   -hs.   `.-/hy/-.  
+echo                      .+yddhyo/-.`               ``.-:ohhd+ods`                  .ho`    `..----..  
+echo                       ``-:+hmddhssoooo+++++oo+syhddmmy/.o``-yds/.`            `-ho`                
+echo                            ./hmhyoosydo---:sy--+Nmy:+hh+y-`.oyNhyyys/-`      `+ho`                 
+echo                              `:y+```.-+/`  `/+:hmhy.`-dys+.o/-oo.-:ohdho:.`./yy:`                  
+echo                               `.+o-`  `y/```.hdhdhy:`:h.`o:.-+/s:````-/yhhyhs:``                   
+echo                                 `oy/.  +yoy+`so.-sdh/sm-..+:``+ss+`    ``.-.`                      
+echo                                 `-mh+` ``-o- o/`..smhyyh/`.o+.+/-d+`                               
+echo                                ``+NNh/  `so``++`..ydy-`/d:..sh: `+m:                               
+echo                                 /myyms.  `/:--o...dhs` `/h-`-s` ``sy.                              
+echo                                .ho`-mho`  ``:+//`-mhs`  `s+`.o/   `ys`                             
+echo                               `sh. `yhs`    `:sh-.yhy-` `.o``/o`   :m.                             
+echo                               /m/  `sds`      ./o--dho`  `o.-o+`   `s/                             
+echo                              .hy`   ods`        -o-+dh+``:y/-o:`    .h.                            
+echo                              +m-   `ydh+ooo:....-/hsoosoh+``.s`      +/                            
+echo                             `do`-:oymNd+osdNdhhy:`.o/.``//``//       :o                            
+echo                             :Nyhdy/:dh/sy++/-.`   ``.-://o-+:`       .y.                           
+echo                            `/ys/.  -my:o-              `-/oy.        `y+                           
+echo                               `    +Nh:o.                 `-.        `oy`                          
+echo                                   `os+-.`                             -o`           
+echo                                                          LulzSec Reborn Like Never Before... Ferda LULZ
+set /p IP=IP:
+:top
+PING -n 1 %IP% | FIND "TTL="              
+IF ERRORLEVEL 1 (SET in=0a & echo %IP% Host is not recieving packets.....) ELSE (SET in=4)
+ping -t 2 0 1 127.0.0.1 >nul
+color 02
+ping -t 2 0 1 127.0.0.1 >nul
+color 03
+ping -t 2 0 1 127.0.0.1 >nul
+color 04
+ping -t 2 0 1 127.0.0.1 >nul
+color 05
+ping -t 2 0 1 127.0.0.1 >nul
+color 06 
+ping -t 2 0 1 127.0.0.1 >nul
+color 07
+ping -t 2 0 1 127.0.0.1 >nul
+color 08
+ping -t 2 0 1 127.0.0.1 >nul
+color 09
+ping -t 2 0 1 127.0.0.1 >nul
+color 0A 
+ping -t 2 0 1 127.0.0.1 >nul
+color 0B
+ping -t 2 0 1 127.0.0.1 >nul
+color 0C
+ping -t 2 0 1 127.0.0.1 >nul
+color 0D
+ping -t 2 0 1 127.0.0.1 >nul
+color 0F
+ping -t 2 0 1 127.0.0.1 >nul
+GoTo top
